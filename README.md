6f45926425ddec7922dc9b873e9a29f58cc64e9c
paullecodeur <paulyemdji@gmail.com>
2021-07-09 16:24:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-tablereorder-demo-ecaerb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-tablereorder-demo-ecaerb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './productservice';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent { 
+    products: Product[];
+
+    cols: any[];
+
+    constructor(private productService: ProductService) { }
+
+    ngOnInit() {
+        this.productService.getProductsSmall().then(data => this.products = data);
+
+        this.cols = [
+            { field: 'code', header: 'Code' },
+            { field: 'name', header: 'Name' },
+            { field: 'category', header: 'Category' },
+            { field: 'quantity', header: 'Quantity' }
+        ];
+    }
+}
